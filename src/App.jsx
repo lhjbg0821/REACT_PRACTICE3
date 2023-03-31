c1c3b0bce5ae98ba0e6515a76893dda584aa69fd
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-03-31 11:21:06 +0900

    onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,46 @@
 import { useState } from "react";
 
-// let noUseState = 0;
-
-// noUseState = 1;
 const App = () => {
-  const [count, setCount] = useState(0);
-  const onClickAdd = () => {
-    setCount(count + 1);
+  const [num1, setNum1] = useState();
+  const [result, setResult] = useState(0);
+
+  const onChangeNum1 = (e) => {
+    console.log(e.target.value);
   };
-
-  const onClickSubtract = () => {
-    // setCount((prev) => {
-    //   return prev - 1;
-    // });
-
-    if (count <= 0) {
-      alert("count의 값이 0보다 작을 수 없습니다!");
-      return;
-    }
-    setCount(count - 1);
-  };
-
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <div>{count}</div>
-      <button
-        className="bg-green-300 w-12 h-12 rounded-full"
-        onClick={onClickAdd}
-      >
-        +
-      </button>
-      <button
-        className="bg-green-300 w-12 h-12 rounded-full"
-        onClick={onClickSubtract}
-      >
-        -
-      </button>
+      <h1 className="text-2xl font-bold mb-4">
+        Awesome한 계산기..로 바뀔 예정
+      </h1>
+      <div className="text-3xl font-black mb-4">{result}</div>
+      <div className="mb-4">
+        <input
+          className="border-2 focus:outline-none focus:border-puple-400 rounded-lg px-4 py-2 text-xl"
+          type="text"
+          value={num1}
+          onChange={onChangeNum1}
+        />
+        <input
+          className="border-2 focus:outline-none focus:border-puple-400 rounded-lg px-4 py-2 text-xl ml-4"
+          type="text"
+        />
+      </div>
+      <div>
+        <button className="border-2 px-2 py-1 rounded-lg border-red-500 text-red-500 ">
+          Add
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-blue-500 text-blue-500 ml-4">
+          Subtract
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-yellow-500 text-yellow-500 ml-4">
+          Multiply
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-green-500 text-green-500 ml-4">
+          Divide
+        </button>
+      </div>
     </div>
   );
 };
-
-// const onClickAdd = () => {
-//   // noUseState = noUseState + 1;
-//   console.log("Clicked!");
-//   noUseState += 1;
-// };
 
 export default App;
